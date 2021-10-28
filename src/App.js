71a5c953266a58f23c80bb8a9b31d821f57807a1
Kun <yukun0620@gmail.com>
2021-10-29 01:50:51 +0800

    i18n

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from "react";
+import { useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 
-function App() {
+const MyComponent = () => {
+  const { t, i18n } = useTranslation()
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng)
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{t('hello')}</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Trans i18nKey='link'>
+        一起來
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
         >
-          Learn React
+           這裡
         </a>
+        學習 React 吧
+        </Trans>
+        <div>
+          <button onClick={() => changeLanguage('zh-tw')}>中文</button>
+          <button onClick={() => changeLanguage('en')}>English</button>
+          <button onClick={() => changeLanguage('es')}>Español</button>
+        </div>
       </header>
     </div>
-  );
+  )
+}
+
+function App() {
+   return (
+      <Suspense fallback="loading">
+         <MyComponent />
+      </Suspense>
+   );
 }
 
 export default App;
